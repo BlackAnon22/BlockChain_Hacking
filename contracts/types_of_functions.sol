e1eeb4a676a377522bfb732d4013d3e8a383d736
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2023-07-02 08:36:33 +0100

    Create types_of_functions.sol

---
+++ contracts/types_of_functions.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract myContract{
+    uint public number1;
+
+    function myFunction() public {
+        uint num; // you don't have to declare a visibility modifier for variables inside functions
+    }
+    function newFunction() public returns (uint, address){
+        uint num1 = 12;
+        uint num2 = 13;
+        number1 = num1 + num2;
+        address myAddress = msg.sender;
+        return (number1,myAddress);
+
+
+    }
+
+}

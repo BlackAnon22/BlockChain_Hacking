59f16ccd7cfeb61d44f947b23f590ebef843aa5c
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2023-07-02 08:28:38 +0100

    Create functions.sol

---
+++ contracts/functions.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract myContract {
+    
+    uint public num; //by default num is set to zero
+
+    function viewNum() public view returns (uint){ // we are trying to read the vallue of the state variable
+        return num; // this returns the value of the  state variable
+    }
+
+    function addNumbers(uint a, uint b) public pure  returns (uint){ // this function is not reading any variable from the contract, it is doing its stuff in it's function. This is why you use the "pure" keyword
+        uint c= a + b;
+        return c;
+    }
+}

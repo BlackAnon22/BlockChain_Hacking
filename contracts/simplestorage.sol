921c5f6727956624b992c7bd669b8e696b9b1d95
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2023-07-05 15:47:16 +0100

    Create simplestorage.sol

---
+++ contracts/simplestorage.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.19; //solidity version
+
+contract Storage{
+
+    //this defaults to 0
+    uint256 MyFavoriteNumber;
+
+    //creating a new type "Person"
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    //creating a dynamic array
+    Person[] public listOfPeople;
+
+    //creating a dictionary
+    mapping(string => uint256) public nameOfFavoriteNumber;
+
+    //declaring a function store
+    function store(uint256 _favoriteNumber) public{
+        MyFavoriteNumber = _favoriteNumber;
+    }
+    
+    function retrieve() public view returns(uint256){
+        return MyFavoriteNumber; 
+    }
+
+    //creating a function that pushes new people into the array and dictionary
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber,_name));
+        nameOfFavoriteNumber[_name] = _favoriteNumber;
+    }
+}

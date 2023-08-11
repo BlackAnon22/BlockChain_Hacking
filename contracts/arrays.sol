1544b8d0b1f4da1e3e03137c5163a3f13f6cc7ef
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2023-08-11 13:39:02 +0100

    Create arrays.sol

---
+++ contracts/arrays.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.5.0;
+
+contract test{
+    function testArray() public pure{
+        uint len = 7;
+    
+        //creating a dynamic array
+        uint[] memory a = new uint[](7);
+
+        //bytes is the same as byte[]
+        bytes memory b = new bytes(len);
+
+        assert(a.length == 7);
+        assert(b.length == len);
+
+        //access array variable
+        a[6] == 9;
+
+        //test array variable
+        assert(a[6] == 9);
+
+        //static array
+        uint[3] memory c = [uint(1), 2, 3];
+        assert(c.length == 3);
+    }
+}

7bf048599d68d46595ceb5e57c77aac1a5932c3a
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2023-12-30 19:07:06 +0100

    Update import.sol

--- contracts/import.sol
+++ contracts/import.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 import{SimpleStorage} from "./SimpleStorage.sol"; // with this we can import different contracts 
 
 contract StorageFactory{
-    SimpleStorage public simpleStorage;
+    SimpleStorage public simpleStorage; // "SimpleStorage" is refers to the contract, "simpleStorage" refers to the variable
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        simpleStorage = new SimpleStorage(); // deployes a new contract
     }
 }
 

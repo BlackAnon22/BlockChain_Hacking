68295beae331c847960b4fe34eb4bc70a54ef131
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-01-02 18:02:45 +0100

    Create payable.sol

---
+++ codes/payable.sol
@@ -0,0 +1,16 @@
+// The "payable" keyword allows a function to accept the native blockchain currency
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract FundMe {
+
+    // To send money to our contract
+    // Allow users to send $
+    // Have a mininum $ sent, $5
+    function fund() public payable  { 
+        msg.value;
+
+    }
+}

fd6cfb709d4456ac5774b502d5f2abdb1a0e65ba
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-01-02 18:09:03 +0100

    Update require.sol

--- codes/require.sol
+++ codes/require.sol
@@ -1,1 +1,16 @@
-// Lets say we want users to send atleast 1 ETH in the contract we can use the "require" keyword
+// Lets say we want users to send atleast 1 ETH in the contract we can use the "require" keyword, the keyword is more like a checker
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract FundMe {
+
+    // To send money to our contract
+    // Allow users to send $
+    // Have a mininum $ sent, $5
+    function fund() public payable  { 
+        require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000 Gwei = 1000000000000000000 Wei
+
+    }
+}

ed2399eafdaa7c31b042063107dd5186f635992f
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-01-06 12:42:32 +0100

    Create SafeMath.sol

---
+++ codes/SafeMath.sol
@@ -0,0 +1,16 @@
+// This is one of the most common library that was used for a long time
+
+// This works for a version below 0.8.0, so when you add 1 to 255, since we can't have 256 it'll start from the lowest number which is zero. 
+// Deploying this with a version 0.8.0, actually gives an error, so it basically doesn't add it
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract SafeMathTester {
+
+    uint8 public bigNumber = 255; // 255 is the biggest number that can be fit in a uint8
+
+    function add() public {
+        bigNumber = bigNumber + 1; // this works for solidity versions before 0.8.0
+    }
+}

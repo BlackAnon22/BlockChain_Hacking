a3b0a72dac9ca024d140d43d666fd93ae702d676
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-01-11 07:03:35 +0100

    Update fundMe.sol

--- contracts/contract_2/fundMe.sol
+++ contracts/contract_2/fundMe.sol
@@ -30,7 +30,7 @@ contract FundMe {
     // Allow users to send $
     // Have a mininum $ sent, $5
     function fund() public payable  { 
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "did not send enough ETH");
         funders.push(msg.sender);
         //addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
         addressToAmountFunded[msg.sender] += msg.value; // this is more condensed 

22a83d919e2de85df6f796623d5d52897e29ce94
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-01-25 17:49:27 +0100

    Create deploy.s.sol

---
+++ scripts/deploy.s.sol
@@ -0,0 +1,21 @@
+// Deploying a contract using a script
+
+// PS: This is a script not a contract
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+// inherits from Script
+contract DeploySimpleStorage is Script { 
+    
+    function run() external returns (SimpleStorage) { // returns the contract address
+        vm.startBroadcast(); // start broadcasting
+        SimpleStorage simpleStorage = new SimpleStorage(); // The "new" keyword is used to deploy a different contract
+        vm.stopBroadcast(); // stop broadcasting
+        return simpleStorage; 
+    }
+}

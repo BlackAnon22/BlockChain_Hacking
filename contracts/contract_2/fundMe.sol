214c2da6ec62bab6e4209aadc96e17cd0fc3a64e
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-01-25 15:59:20 +0100

    Update fundMe.sol

--- contracts/contract_2/fundMe.sol
+++ contracts/contract_2/fundMe.sol
@@ -4,6 +4,7 @@
 
 pragma solidity ^0.8.0;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import{PriceConverter} from "./PriceConverter.sol";
 
 error NotOwner();

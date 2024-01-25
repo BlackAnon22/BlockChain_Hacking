c24fda605082d35dea9a460d30e0fa865f1f2256
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-01-25 17:47:14 +0100

    Create test_1.t.sol

---
+++ tests/test_1.t.sol
@@ -0,0 +1,20 @@
+// This is a basic test to show how the "setUp()" function is executed before any other function
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+
+contract FundMeTest is Test {
+    uint256 number = 1;
+
+    // intended to be executed before any test function, here is where we will deploy our contracts
+    function setUp() external {
+        number = 2;
+    }
+
+    function testDemo() public {
+        assertEq(number, 2);
+    }
+}

2c467fcb1800c64cb85c6581edfccf4aa3d95f49
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-01-26 17:14:17 +0100

    Update test_3.t.sol

--- tests/test_3.t.sol
+++ tests/test_3.t.sol
@@ -1,3 +1,6 @@
+// This is another test contract
+// This test contract  sets up an instance of the FundMe contract and includes a test function to verify that the MINIMUM_USD constant in the FundMe contract is set to 5 ether
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;

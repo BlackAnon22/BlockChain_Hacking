fce9b4b792ce3aee8eaa70262bb40a0915a6d060
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-01-29 06:17:43 +0100

    Update fundMe.sol

--- contracts/contract_2/fundMe.sol
+++ contracts/contract_2/fundMe.sol
@@ -57,7 +57,7 @@ contract FundMe {
 
        // call
        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
-       require(callSuccess, "Call Failed"); // requiring callSuccess is true, other wise prints an error that says "call failed"
+       require(callSuccess, "Call Failed"); // requiring callSuccess is true, other wise prints an error message that says "call failed"
         
     }
 

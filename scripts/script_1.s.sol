3ea84ceef539967505ed7e37aef518185585e828
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-02-06 16:18:14 +0100

    Update script_1.s.sol

--- scripts/script_1.s.sol
+++ scripts/script_1.s.sol
@@ -4,7 +4,7 @@
 
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18; 
+pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";

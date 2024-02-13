1fa31bcf9d519d1e36c587882656dac7942898a6
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-02-13 18:38:28 +0100

    Update test_3.t.sol

--- tests/test_3.t.sol
+++ tests/test_3.t.sol
@@ -1,12 +1,10 @@
-// This is another test contract
-// This test contract  sets up an instance of the FundMe contract and includes a test function to verify that the MINIMUM_USD constant in the FundMe contract is set to 5 ether
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     // declares a state variable fundme of type FundMe
@@ -14,11 +12,22 @@ contract FundMeTest is Test {
 
     // intended to be executed before any test function, here is where we will deploy our contracts
     function setUp() external {
-        // deploys a new instance of the FundMe contract and assigns it to the fundme state variable
-        fundme = new FundMe();
+        // deploys a new instance of the FundMe contract and assigns it to the fundme state variable.
+        //fundme = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundme = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public {
         assertEq(fundme.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundme.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundme.getVersion();
+        assertEq(version, 0);
     }
 }

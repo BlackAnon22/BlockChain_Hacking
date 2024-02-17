b90efc81d07db63fd578e67a0cb71d673b358c46
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-02-17 19:55:13 +0100

    Update override.sol

--- codes/override.sol
+++ codes/override.sol
@@ -8,7 +8,7 @@ contract AddFiveStorage is SimpleStorage { //inherits the functionalites of the 
 
     //the override keyword here is used to override the store() function in the simplestorage contract, the virtual keyword is added to the store() function in the simplestorage contract
     function store(uint256 _newNumber) public override { 
-        MyFavoriteNumber = _newNumber + 5; //calls the variable "MyFavoriteNumber" from the simplestorage contract and then adds 5 to it
+        MyFavoriteNumber = _newNumber + 5; // calls the variable "MyFavoriteNumber" from the simplestorage contract and then adds 5 to it
     }
 
 }

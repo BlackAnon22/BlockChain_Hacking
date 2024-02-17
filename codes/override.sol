f139211cf4ed0c9c9f4fdf80093e2800edb3114b
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-02-17 19:55:43 +0100

    Update override.sol

--- codes/override.sol
+++ codes/override.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import {SimpleStorage} from "./SimpleStorage.sol"; //imports the SimpleStorage contract
 
-contract AddFiveStorage is SimpleStorage { //inherits the functionalites of the simplestorage contract
+contract AddFiveStorage is SimpleStorage { // inherits the functionalites of the simplestorage contract
 
     // the override keyword here is used to override the store() function in the simplestorage contract, the virtual keyword is added to the store() function in the simplestorage contract
     function store(uint256 _newNumber) public override { 

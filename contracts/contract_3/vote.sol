6cab18153d04255adbcdf7d528c07ca1baebe27c
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-02-29 20:15:17 +0100

    Update vote.sol

--- contracts/contract_3/vote.sol
+++ contracts/contract_3/vote.sol
@@ -1,1 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract voteCharacters {
+    string public animeCharacter;
+
+    struct Person {
+        string favoriteAnimeCharacter;
+        string name;
+    }
+
+    // dynamic array of Person
+    Person[] public listOfPeople;
+
+    mapping(string => string) public nameToFavoriteAnimeCharacter;
+
+    function store(string memory _favoriteAnimeCharacter) public {
+        animeCharacter = _favoriteAnimeCharacter;
+    }
+
+    function retrieve() public view returns (string memory) {
+        return animeCharacter;
+    }
+
+    function addPerson(
+        string memory _name,
+        string memory _favoriteAnimeCharacter
+    ) public {
+        listOfPeople.push(Person(_favoriteAnimeCharacter, _name));
+        nameToFavoriteAnimeCharacter[_name] = _favoriteAnimeCharacter;
+    }
+}
 

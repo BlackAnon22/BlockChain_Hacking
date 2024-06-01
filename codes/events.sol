dfd58660e2c8561eaa7e6d6f01ce8d7993be156b
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-06-01 08:35:53 +0100

    Update events.sol

--- codes/events.sol
+++ codes/events.sol
@@ -2,12 +2,13 @@
 
 pragma solidity ^0.8.19;
 
-event HigghestPayBidder(address bidder, uint amount);
+// This declares an event named "HighestBidIncreased" that logs the bidder's address and the bid amount.
+event HigghestPayBidder(address bidder, uint amount); // creating an event
 
 contract SimpleAuction {
 
     function bid() public payable {
-        emit HigghestPayBidder(msg.sender, msg.value);
+        emit HigghestPayBidder(msg.sender, msg.value); // triggering the event
     }
     
 

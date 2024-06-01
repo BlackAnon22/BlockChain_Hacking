22938473f2aacb5fece804db67e06495784f52e1
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-06-01 21:32:30 +0100

    Update gas.sol

--- codes/gas.sol
+++ codes/gas.sol
@@ -6,8 +6,13 @@ contract Gas {
 
     uint256 public random = 0;
 
+     // Using up all of the gas that you send causes your transaction to fail.
+    // State changes are undone.
+    // Gas spent are not refunded.
     function forever() public {
 
+        // Here we run a loop until all of the gas are spent
+        // and the transaction fails
         while (true) {
             random += 1;
         }

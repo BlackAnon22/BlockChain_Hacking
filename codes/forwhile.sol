3901428bc91c977d64459486b946a8e8243a4dcf
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-06-02 11:14:19 +0100

    Update forwhile.sol

--- codes/forwhile.sol
+++ codes/forwhile.sol
@@ -6,6 +6,7 @@ contract forWhile {
     function loop() public pure {
         for (uint256 i = 0; i < 10; i++) {
             if (i == 3) {
+                // skip and continue to the next iteration
                 continue;
             }
             if (i == 5) {

78250ea12f4fc5a93f78ed08501a15490c6855c7
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-06-02 11:12:18 +0100

    Create forwhile.sol

---
+++ codes/forwhile.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+contract forWhile {
+    function loop() public pure {
+        for (uint256 i = 0; i < 10; i++) {
+            if (i == 3) {
+                continue;
+            }
+            if (i == 5) {
+                
+                break;
+            }
+
+            // while loop
+            uint256 j;
+            while (j < 10) {
+                j++;
+            }
+            
+        }
+    }
+}

35a9068312232d4076dddf772d193ef50f45a985
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-06-04 11:21:01 +0100

    Create transient_storage.sol

---
+++ codes/transient_storage.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+contract TransientStorage {
+    uint256 public persistentData;
+
+    // Function that uses memory for transient storage
+    function usetransientStorage(uint256 len) public pure returns(bytes memory) {
+        // Transient storage
+        bytes memory temporaryData = new bytes(len);
+
+        // Do something with temporaryData
+        for(uint256 i = 0; i < len; i++) {
+            temporaryData[i] = bytes1(uint8(i));
+        }
+
+        return temporaryData;
+    }
+}

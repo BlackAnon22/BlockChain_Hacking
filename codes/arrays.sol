4986971a8fc72cadd5d5420663b9bad59425c7b7
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-06-20 19:27:17 +0100

    Update arrays.sol

--- codes/arrays.sol
+++ codes/arrays.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.5.0;
 
 contract test{
-    function testArray() public pure{
+    function testArray() public pure{ 
         uint len = 7;
     
         //creating a dynamic array

1461bd81204108f8d85cb0250ed2b897ea789d3d
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-06-25 07:46:28 +0100

    Update MockV3Aggregator.sol

--- mocks/MockV3Aggregator.sol
+++ mocks/MockV3Aggregator.sol
@@ -11,7 +11,7 @@ import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     // If we are on a local anvil chain, deploy mocks
-    // Otherwise, grab the existing address from the live network
+    // Otherwise, grab the existing address from the live network 
 
     NetworkConfig public activeNetworkConfig;
 

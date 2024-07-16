8b5a982a1d4337df08cb3fec13cd12d796b00f49
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-07-16 16:22:41 +0100

    Update test_1.t.sol

--- tests/test_1.t.sol
+++ tests/test_1.t.sol
@@ -1,7 +1,7 @@
 // This is a basic test contract to show how the "setUp()" function is executed before any other function
 
 // SPDX-License-Identifier: MIT
-
+ 
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";

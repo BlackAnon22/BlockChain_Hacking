ddc2ff46218269b083d67f21f24ac1122f5d8655
bl4ck4non <67879936+BlackAnon22@users.noreply.github.com>
2024-08-15 18:05:47 +0100

    Update SafeMath.sol

--- codes/SafeMath.sol
+++ codes/SafeMath.sol
@@ -13,7 +13,7 @@ contract SafeMathTester {
     uint8 public bigNumber = 255; // unchecked
     function add() public {
         bigNumber = bigNumber + 1; // this works for solidity versions before 0.8.0
-    }
+    } 
 }
 
 
